19b97c329e577f21c3414f48e1d2eb7e5ea4e589
StellaOnyekwelu <105196222+StellaOnyekwelu@users.noreply.github.com>
2022-06-09 19:19:12 +0100

    CLI calculator tasks

---
+++ Cli-Calculator.py
@@ -0,0 +1,17 @@
+num1 = int(input('Enter First Number'))
+num2 = int(input('Enter Second Number'))
+
+#Addition
+print(num1 + num2)
+
+#Subtraction
+print(num1 - num2)
+
+#Division
+print(num1 / num2)
+
+#Multiplication
+print(num1 * num2)
+
+#Modulus
+print(num1 % num2)
